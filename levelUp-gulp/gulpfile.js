4c85ef1233769d83ee4d4def407fadaae7f8702d
Traian <niculescu.traian.ct@gmail.com>
2020-03-29 01:43:22 +0200

    update

---
+++ levelUp-gulp/gulpfile.js
@@ -0,0 +1,65 @@
+let gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat');
+
+/*
+TOP LEVEL FUNCTIONS
+gulp.task - define tasks
+gulp.src - point to files to use
+gulp.dest - point to folder to output
+gulp.watch - watch files and folders for changes
+*/
+/*
+gulp.task('message', function() {
+  return new Promise((resolve, reject) => {
+    console.log('Hello World! from gulp');
+    resolve();
+  });
+});
+*/
+gulp.task('message', async function() {
+  console.log('gulp is running...');
+});
+
+// gulp.task('uglify', async () => {
+//   gulp
+//     .src('src/js/*.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('dist/js'));
+// });
+
+// copy all html files
+gulp.task('copyHtml', async () => {
+  gulp.src('src/*.html').pipe(gulp.dest('dist'));
+});
+
+// optimize images
+gulp.task('imagemin', async () => {
+  gulp
+    .src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+});
+
+// compile sass
+gulp.task('sass', async () => {
+  gulp
+    .src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', async () => {
+  gulp
+    .src('src/js/*.js')
+    .pipe(gulp.concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task(
+  'default',
+  gulp.parallel(['message', 'copyHtml', 'imagemin', 'sass', 'scripts'])
+);
---
+++ levelUp-gulp/src/js/file2.js
@@ -0,0 +1,7 @@
+// Console Log
+
+console.log('This is file Two');
+
+// Alert
+
+alert('This is file 2');
